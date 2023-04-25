59c6c5f92c0363980e7e60d82f13bbe45cc84ee2
Leticia Santos Moura <letistos@gmail.com>
2023-04-25 13:37:42 -0300

    seleção itens

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-let nome = prompt("qual seu nome?");
+const nome = prompt("qual seu nome?");
 console.log(nome);
 
-function selecionarOpcao(){
+
+function selecionar(){
+
+const selecionei = document.querySelectorAll('.roupa.selecionado')
+
+    for(i=0; i< selecionei.length; i++){
+        if(selecionei[i] !== null){
+        selecionei[i].classlist.remove('.selecionado')
+        }
+    }
+
+    let vouSelecionar = document.querySelector(this)
+    vouSelecionar.classList.add('selecionado');
     
+    
+    //selecionei.classList.add('selecionado'); 
+    console.log(selecionei);
+}
+//function confirmaBotao()
+
+function imgReferencia (){
+
+    const input = document.querySelector('.link').target.value;
+   // linkref = input.innerHTML.value;
+    console.log(input);
 }
